0800099aa19fcef3d760257a2ee75a70fa8ae4cc
Aryan <aryan.gupta@inmobi.com>
2021-08-26 14:42:50 +0530

    Changed the UI of Publisher

--- Publisher/Publisher/ViewController.swift
+++ Publisher/Publisher/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import InMobiSyndication
 
+
 class ViewController: UIViewController {
 
     var controller: InMobiSyndicationViewController?;
@@ -17,22 +18,22 @@ class ViewController: UIViewController {
         
     }
 
-    @IBAction func showAd(_ sender: UIButton) {
-        // Do any additional setup after loading the view.
-        controller = InMobiSyndicationViewController();
-        controller?.setPlacementId(placementId: 1443100076943726)
-        controller?.adFinishedLoading = {
-            self.controller?.showInterstitialAd()
-        }
-
-        controller?.adErrorLoading = {
-            print("Error here")
-        }
-        
-        controller?.view.backgroundColor = .orange
-        self.present(controller ?? UIViewController(), animated: false, completion: nil)
-        
-    }
+//    @IBAction func showAd(_ sender: UIButton) {
+//        // Do any additional setup after loading the view.
+//        controller = InMobiSyndicationViewController();
+//        controller?.setPlacementId(placementId: 1443100076943726)
+//        controller?.adFinishedLoading = {
+//            self.controller?.showInterstitialAd()
+//        }
+//
+//        controller?.adErrorLoading = {
+//            print("Error here")
+//        }
+//
+//        controller?.view.backgroundColor = .orange
+//        self.present(controller ?? UIViewController(), animated: false, completion: nil)
+//
+//    }
     
     
 }
